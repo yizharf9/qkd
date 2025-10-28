27b5903bb9ef0648d0449ae0ce65e9ca56e109af
yizhar <yizharf9@gmail.com>
2025-10-28 18:33:44 +0200

    single_simulation plot matrix, masked field plot fully functional

--- oct_2025/massive_simulation.py
+++ oct_2025/massive_simulation.py
@@ -36,7 +36,6 @@ start = 0.01
 end = 0.15
 r0_list = np.linspace(start=start,stop=end,num=num_of_r0_samples,)
 
-<<<<<<< HEAD
 # wavelength values
 wavelengths = [1.55e-6, 0.500e-6]
 
@@ -44,11 +43,6 @@ wavelengths = [1.55e-6, 0.500e-6]
 N = 4
 count = 1
 for wavelength in wavelengths :
-=======
-N = 100
-N1=2
-for wavelength in [1.55e-6, 0.500e-6] : #run with turbulence 100 {N} runs
->>>>>>> 80d6f64 (change massive run  to add a non turb data)
     for r0_ref in r0_list :
         for run_number in range(1,N+1):  # example run number
             ns = {
@@ -58,34 +52,9 @@ for wavelength in [1.55e-6, 0.500e-6] : #run with turbulence 100 {N} runs
                     "wavelength": wavelength,
                     "r0_ref": r0_ref,  # example value for r0_ref
                     "save_images": save_images,  # example value for r0_ref
-<<<<<<< HEAD
                     "TurbulencLayer": TurbulencLayer,
                 }
             print(f"\n=== run_number={count - 1} finished out of {len(r0_list) * len(wavelengths) * N  } ({count/(len(r0_list) * len(wavelengths) * N  ) * 100:.3f}%) ===")
-=======
-                    "TurbulencLayer": True,
-               }
-
-            print(f"\n=== run_number={run_number} ===")
->>>>>>> c30cd50 (look for what_is_new.ipynb)
             exec(code_obj, ns,ns)  # fresh globals per run (no locals dict)
-<<<<<<< HEAD
             count += 1
 
-=======
-
-for wavelength in [1.55e-6, 0.500e-6] : #run without turbulence 2 {N1} runs
-            for run_number in range(1,N1+1):  # example run number
-                ns = {
-                        "__name__": "__main__",
-                        "__file__": str("single_simulation.py"),   # so Path(__file__) works inside turblance.py
-                        "run_number": run_number,
-                        "wavelength": wavelength,
-                        "r0_ref": r0_ref,  # example value for r0_ref
-                        "save_images": save_images,  # example value for r0_ref
-                        "TurbulencLayer": False,
-                }
-
-                print(f"\n=== run_number={run_number} ===")
-                exec(code_obj, ns,ns)  # fresh globals per run (no locals dict)
->>>>>>> 80d6f64 (change massive run  to add a non turb data)
